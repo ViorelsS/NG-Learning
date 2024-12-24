c19533a8008d0373f53255b50a1cccb9a515cf69
ViorelS <viorel.strogoteanu@dedagroup.it>
2024-12-24 16:57:10 +0100

    Property binding

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -11,40 +11,35 @@ import {
 
 @Component({
   standalone: false,
-  selector: 'viorelss-prova',
+  selector: 'app-prova',
   templateUrl: 'prova.component.html',
   styleUrl: 'prova.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProvaComponent
-  implements
-    OnInit,
-    AfterContentChecked,
-    AfterContentInit,
-    AfterViewInit,
-    DoCheck,
-    OnDestroy
-{
+export class ProvaComponent implements OnInit {
+  isDisabled = false;
+  immagine = '';
+  immagine1 =
+    'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg';
+  immagine2 = 'https://material.angular.io/assets/img/examples/shiba2.jpg';
+
   constructor() {
     console.log('constructor');
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
-  }
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked');
-  }
-  ngAfterContentInit(): void {
-    console.log('ngAfterContentInit');
-  }
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-  }
-  ngDoCheck(): void {
-    console.log('ngDoCheck');
-  }
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy');
+
+    let counter = 0;
+    setInterval(() => {
+      if (counter % 2 === 0) {
+        this.immagine = this.immagine1;
+      } else {
+        this.immagine = this.immagine2;
+      }
+      counter++;
+
+      this.isDisabled = !this.isDisabled;
+      console.log(this.isDisabled);
+    }, 1000);
   }
 }
