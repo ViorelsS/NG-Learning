584663a3558200e3e9d59100872696592c0391e0
ViorelS <viorel.strogoteanu@dedagroup.it>
2024-12-24 16:27:30 +0100

    Component lifecycle methods

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   standalone: false,
@@ -7,4 +16,35 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: 'prova.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProvaComponent {}
+export class ProvaComponent
+  implements
+    OnInit,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewInit,
+    DoCheck,
+    OnDestroy
+{
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+}
