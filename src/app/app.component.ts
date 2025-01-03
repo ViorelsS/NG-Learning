954da6ca27788f10eaf3578cdd9bdf16f31dcf61
ViorelS <viorel.strogoteanu@dedagroup.it>
2025-01-03 10:06:45 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'corso-angular';
+  isVisibile = true;
 
   onInput(event: Event) {
     this.title = (event.target as HTMLInputElement).value;
