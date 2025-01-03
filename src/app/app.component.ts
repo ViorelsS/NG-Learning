3ae3d02ab25b4a7f445982c5a597e0ce9baad8c8
ViorelS <viorel.strogoteanu@dedagroup.it>
2025-01-03 10:24:30 +0100

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'corso-angular';
   isVisibile = true;
+  persone = [
+    { nome: 'Mario', cognome: 'Rossi', isOnline: true },
+    { nome: 'Paolo', cognome: 'Verdi', isOnline: false },
+    { nome: 'Luca', cognome: 'Bianchi', isOnline: true },
+    { nome: 'Giovanni', cognome: 'Neri', isOnline: false },
+    { nome: 'Giuseppe', cognome: 'Gialli', isOnline: true },
+  ];
+  numero = 3;
 
   onInput(event: Event) {
     this.title = (event.target as HTMLInputElement).value;
